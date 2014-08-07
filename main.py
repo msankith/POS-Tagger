efd7440e53d695e2347b276c4c5e6cee0339884b
sanjeevmk <sanjeevmk4890@gmail.com>
2014-08-07 22:56:09 +0530

    main.py : This contains the test in sequence. It calls all other modules createMatrices.py : Added bigram_trainer() wrapper statistics.py : Writing matrices to csv added trigram.py : Added code to return trans and emit matrices on trigrams validation.py : kfold implementation completed, with writing of reports

---
+++ main.py
@@ -0,0 +1,54 @@
+from createMatrices import bigram_trainer
+from trigram import trigram_trainer
+from validation import wordsfromtest
+from statistics import accuracy
+from statistics import csv_reports
+from sys import argv
+
+corpusfile = argv[1]
+inputfile = argv[2]
+outputfile = argv[3]
+basefile = argv[4]
+
+# Step 1 - Train using bigrams or trigrams on the corpusfile
+transition, emission = bigram_trainer(corpusfile)
+#transition, emission = trigram_trainer(corpusfile)
+
+# Step 2 - Get the word list from the input file
+words = wordsfromtest(inputfile)
+
+# Step 3 - Pass the word list and matrices to viterbi implementation
+#prediction = viterbi(words,transition,emission)
+
+prediction = "kfold/base1.txt"
+
+''' 
+viterbi() should return the PATH of the output file. 
+Output file contains the predicted tags for words in the same format as the corpus
+word1_tag1 word2_tag2 word3_tag3 and so on 
+'''
+
+# Step 4 - Get accuracy related statistics
+
+stats = accuracy(prediction,basefile)
+# the prediction would be compared against the basefile
+# this could be the corpusfile itself or, we can use some other manually annotated basefile
+
+stats.setTestSetAsBase() 
+# Use this if you passed a manually annotated basefile for comparison
+
+#stats.setMaxProbabAsBase()
+# Use this if you want to consider maximum probability tag for each word as the correct tag for that word.
+
+accuracy_percent = stats.percentage()
+overall_p , overall_r = stats.precision_recall()
+tags, cm = stats.confusion_matrix()
+prfs = stats.precision_recall_fscore_support()
+
+
+# Step 5 - Generate Reports
+reports = csv_reports()
+
+reports.confusion_csv(tags,cm,outputfile)
+reports.prfs_csv(tags,prfs,outputfile,True)
+reports.accuracy_csv(accuracy_percent,outputfile,True)
